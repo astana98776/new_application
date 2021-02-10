62022d0c9b917220667ecb525e3b2a5f6382a5e0
astana98776 <astana98776@gmail.com>
2021-02-10 12:53:57 +0600

    homework

---
+++ app/src/main/java/com/example/loftmoney/Item.java
@@ -0,0 +1,31 @@
+package com.example.loftmoney;
+
+
+
+
+public class Item {
+
+    private String name;
+    private int price;
+
+    public Item(final String name, final int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(final int price) {
+        this.price = price;
+    }
+}
